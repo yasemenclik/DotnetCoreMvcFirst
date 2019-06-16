957338af34ab3ba3c776d0db32478d4ca447e496
Yasemen ÇeLik <50753447+yasemenclik@users.noreply.github.com>
2019-06-16 11:58:19 +0300

    First Projet created.

---
+++ DotnetCoreMVCFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotnetCoreMVCFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
